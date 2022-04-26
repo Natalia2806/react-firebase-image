22951f78a2bb5911991620c97d4058290797da74
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-04-25 20:28:22 -0500

    Finalizado

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -31,7 +31,7 @@ const Form = () => {
         var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
         return regex.test(email) ? true : false;
     }
-
+//Metodo para obtener imagenes
     const obtenerImagen = async () => {
         try {
             const res = await fetch("https://picsum.photos/200");
